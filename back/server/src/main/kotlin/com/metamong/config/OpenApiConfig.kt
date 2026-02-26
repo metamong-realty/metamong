278b7bfa7041d5e6aa18db53bdf2d893f6885583
Damongsanga <mike0413lata@gmail.com>
2026-02-26 21:45:10 +0900

    openApiConfig

--- back/server/src/main/kotlin/com/metamong/config/OpenApiConfig.kt
+++ back/server/src/main/kotlin/com/metamong/config/OpenApiConfig.kt
@@ -2,12 +2,15 @@ package com.metamong.config
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.servers.Server
+import org.springframework.context.annotation.Configuration
 
-
+@Configuration
 @OpenAPIDefinition(
-    servers = [Server(
-        url = "https://metamong-server-production.up.railway.app",
-        description = "Default Server URL"
-    )]
+    servers = [
+        Server(
+            url = "https://metamong-server-production.up.railway.app",
+            description = "Default Server URL",
+        ),
+    ],
 )
 class OpenApiConfig
