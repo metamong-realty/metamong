5a01a5380a00d413c52d51b792447e7e2deb465e
Damongsanga <mike0413lata@gmail.com>
2026-03-11 10:14:21 +0900

    💚[BE] [버그수정] 동 검색 + 시군구 API (#8)
    동 검색 로직 개선 + 시군구 목록 API 추가

--- back/server/src/main/kotlin/com/metamong/MetamongServerApplication.kt
+++ back/server/src/main/kotlin/com/metamong/MetamongServerApplication.kt
@@ -2,8 +2,10 @@ package com.metamong
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class MetamongServerApplication
 
 fun main(args: Array<String>) {
